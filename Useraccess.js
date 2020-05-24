4ca18922d42e15c731e1b5ec2c4ebe1f07aff0ef
Syed Ziauddin <56580229+ZiaCodes@users.noreply.github.com>
2020-05-24 15:17:30 +0530

    Update Useraccess.js

--- Useraccess.js
+++ Useraccess.js
@@ -1,1 +1,29 @@
+//Camera permission 
 
+
+const video = document.getElementById('video')
+
+function startvideo() {
+	navigator.getUserMedia(
+		{	
+			video: {} },
+		stream => video.srcObject = stream,
+		err => console.error(err)
+  )
+
+}
+
+
+  
+
+startvideo();
+
+
+//User name input
+
+function booster() {
+	var Response =	prompt("please,Type your name!");
+	alert("welcome" + ","+" " +Response + "."+ " " + "Please allow your camera" );
+}
+
+booster(); 
